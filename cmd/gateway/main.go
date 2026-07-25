88e8f666afab3b5e012bde37f1007d0aa63bc130
sopesamuel <fasusimosope@gmail.com>
2026-07-26 00:44:25 +0200

    implement database connection setup and enhance bank interface structure

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"mart-gateway/internal/bank"
+	"database/sql"
+	"flag"
+	"os"
 )
 
 //configuration
@@ -9,4 +12,28 @@ import (
 
 func main() {
 	bank.SendRefundRequestToBank()
+
+	dsn := flag.String("dsn", "web:pass@tcp(localhost:3306)/snippetbox?parseTime=true", "MySQL data source name") 
+
+	_, err := openDB(*dsn)
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+
+func openDB(dsn string)(*sql.DB, error){
+
+	db , err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err 
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
