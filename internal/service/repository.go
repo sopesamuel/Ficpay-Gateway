fb355b5b2966988a08e9d288777aa9034b90ff78
sopesamuel <fasusimosope@gmail.com>
2026-08-09 10:59:04 +0200

    feat: add repository interface for payment and state history management

---
+++ internal/service/repository.go
@@ -0,0 +1,14 @@
+package service 
+
+import (
+	"mart-gateway/internal/models"
+)
+
+type repositoryInterface interface {
+	CreatePayment(payment *models.Payment) error
+	CreateStateHistory(state *models.StateHistory) error
+	UpdatePaymentState(payment *models.Payment) error
+	GetPaymentByID(paymentID string) (models.Payment, error)
+	GetStatusByID(orderID string) (string, error)
+	GetHistoryByCustomerID(customerID string) ([]models.Payment, error)
+}
