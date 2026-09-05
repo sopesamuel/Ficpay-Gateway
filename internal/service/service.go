06442bb205c01c5397c7f84e70a7234f800f90da
sopesamuel <fasusimosope@gmail.com>
2026-09-05 03:40:34 +0200

    feat: implement StateMachine function for state transitions in payment processing

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,5 +1,7 @@
 package service
 
+import "slices"
+
 type MainService struct {
 	bank BankInterface
 	repository RepositoryInterface
@@ -9,4 +11,17 @@ func ActivateService(bankService BankInterface, repo RepositoryInterface) *MainS
 	return &MainService{bank: bankService, repository: repo}
 }
 
-//state machine
+//state machine for communication with the db, checks if 
+
+func StateMachine(from, to string) bool {
+	state := map[string][]string{
+		"PENDING": {"AUTHORIZED", "VOIDED"},
+		"AUTHORIZED": {"CAPTURED", "VOIDED"},
+		"CAPTURED": {"REFUNDED"},
+	}
+
+	if slices.Contains(state[from], to){
+		return true
+	}
+	return false
+}
