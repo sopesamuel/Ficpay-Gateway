fdffab50cbceaa30cff2dd94bfd1c0fb61b69c2e
sopesamuel <fasusimosope@gmail.com>
2026-09-07 15:32:31 +0200

    feat: add uuid dependency and implement AuthorizePayment method in MainService

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,6 +1,12 @@
 package service
 
-import "slices"
+import (
+	"fmt"
+	"mart-gateway/internal/models"
+	"slices"
+
+	"github.com/google/uuid"
+)
 
 type MainService struct {
 	bank BankInterface
@@ -24,4 +30,16 @@ func StateMachine(from, to string) bool {
 		return true
 	}
 	return false
+}
+
+//Authorize to bank from ficmart
+
+func (state MainService) AuthorizePayment(req models.Martrequest, bankreq models.Bankauthrequest, key string) (models.Payment,error){
+	res, err := state.bank.Authorize(bankreq, key)
+	if err != nil{
+		return models.Payment{},  fmt.Errorf("bank authorization failed: %w", err)
+	}
+	
+	auth_id := res.Authorization_id
+	return models.Payment{PaymentID: uuid.New().String(), OrderID: req.Order_id, CustomerID: req.Customer_id,Amount: req.Amount, Currency: "USD", Status: "AUTHORIZED", AuthID: &auth_id, CaptureID: nil, VoidID: nil, RefundID: nil,} , nil
 }
