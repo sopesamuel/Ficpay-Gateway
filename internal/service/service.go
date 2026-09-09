2434aa42e751d0cd75772873e25142bf4fb5c6fe
sopesamuel <fasusimosope@gmail.com>
2026-09-09 23:44:50 +0200

    feat: enhance AuthorizePayment method to streamline payment creation and state history recording

--- internal/service/service.go
+++ internal/service/service.go
@@ -35,25 +35,48 @@ func StateMachine(from, to string) bool {
 //Authorize to bank from ficmart
 
 func (state MainService) AuthorizePayment(req models.Martrequest, bankreq models.Bankauthrequest, key string) (models.Payment,error){
-	//send the request to the bank and we get response 
+	//send the request to the bank TO INITIATE PENDING AND AUTHORIZATION states
+	
+	
+	auth_payment := models.Payment{
+		PaymentID: uuid.New().String(), 
+		OrderID: req.Order_id, 
+		CustomerID: req.Customer_id,
+		Amount: req.Amount, 
+		Currency: "USD", 
+		Status: "PENDING", 
+		}
+
+	err := state.repository.CreatePayment(&auth_payment)
+	if err != nil{
+		return models.Payment{},  fmt.Errorf("starting payment failed (pending state): %w", err)
+	}
+
+	//THEN RECORD THE PENDING STATE
+	err = state.repository.CreateStateHistory(&models.StateHistory{ PaymentID: auth_payment.PaymentID, ToStatus: "PENDING",})
+	if err != nil{
+		return models.Payment{},  fmt.Errorf("failed to store pending state history to stae history db: %w", err)
+	}
+
+	//Then we initiate authorization request to bank
 	res, err := state.bank.Authorize(bankreq, key)
 	if err != nil{
 		return models.Payment{},  fmt.Errorf("bank authorization failed: %w", err)
 	}
-	Payment_ID := uuid.New().String()
-	// we take details from the ficmart request and combine with the respomse from bank(auth id) and store in payments
-	auth_id := res.Authorization_id
-	// save to our payment db 
-	auth_payment := models.Payment{PaymentID: Payment_ID, OrderID: req.Order_id, CustomerID: req.Customer_id,Amount: req.Amount, Currency: "USD", Status: "AUTHORIZED", AuthID: &auth_id, CaptureID: nil, VoidID: nil, RefundID: nil,}
-	err = state.repository.CreatePayment(&auth_payment)
+
+	auth_id_rep := res.Authorization_id
+	pending_state := "PENDING"
+	auth_payment.AuthID = &auth_id_rep
+	auth_payment.Status = "AUTHORIZED"
+
+	err = state.repository.UpdatePaymentState(&auth_payment)
 	if err != nil{
 		return models.Payment{},  fmt.Errorf("failed to store authorization record to payment db: %w", err)
 	}
-
-	authorization_state := models.StateHistory{PaymentID: Payment_ID,FromStatus: nil, ToStatus: "AUTHORIZED" }
-	err = state.repository.CreateStateHistory(&authorization_state)
+	
+	err = state.repository.CreateStateHistory(&models.StateHistory{PaymentID: auth_payment.PaymentID, FromStatus: &pending_state, ToStatus: "AUTHORIZED"})
 	if err != nil{
-		return models.Payment{},  fmt.Errorf("failed to store history authorization record to stae history db: %w", err)
+		return models.Payment{},  fmt.Errorf("failed to store history OF authorization record to stae history db: %w", err)
 	}
 
 	return  auth_payment, nil
