68cd6c548b91ea06789152e4c839268e55490626
sopesamuel <fasusimosope@gmail.com>
2026-09-11 19:00:15 +0200

    add validation for payment state transition in VoidPayment method

--- internal/service/service.go
+++ internal/service/service.go
@@ -121,7 +121,10 @@ func (state MainService) VoidPayment(payment_id string, bankreq models.Bankvoidr
 		return models.Payment{},  fmt.Errorf("failed to get payment reference from db: %w", err)
 	}
 
-	if !StateMachine(res_payment.Status, "VOIDED")
+	if !StateMachine(res_payment.Status, "VOIDED"){
+		return models.Payment{},  fmt.Errorf("Invalid payment transition from: %s to %s",res_payment.Status, "VOIDED")
+	}
+
 
 	void_res, err := state.bank.Void(bankreq, key)
 	if err != nil{
