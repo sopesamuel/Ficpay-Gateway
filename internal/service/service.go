ea1cf6c112cd872f206d8cc2fe1fe6df6376876c
sopesamuel <fasusimosope@gmail.com>
2026-09-21 17:02:02 +0200

    fix: enhance VoidPayment method to handle idempotency key retrieval and storage

--- internal/service/service.go
+++ internal/service/service.go
@@ -142,6 +142,12 @@ func (state *MainService) CapturePayment(payment_id string, bankreq models.Bankc
 }
 
 func (state *MainService) VoidPayment(payment_id string, bankreq models.Bankvoidrequest, key string) (models.Payment, error){
+	existingpaymentID, err := state.repository.GetIdempotencyKey(key)
+	if err == nil {
+		return state.repository.GetPaymentByID(existingpaymentID)
+	} else if err != sql.ErrNoRows {
+		return models.Payment{}, fmt.Errorf("failed to query payment_id of idempotency key from idempotency db): %w", err)
+	}
 	res_payment ,err := state.repository.GetPaymentByID(payment_id)
 	if err != nil{
 		return models.Payment{},  fmt.Errorf("failed to get payment reference from db for void transaction: %w", err)
@@ -168,6 +174,11 @@ func (state *MainService) VoidPayment(payment_id string, bankreq models.Bankvoid
 	err = state.repository.CreateStateHistory(&models.StateHistory{PaymentID: res_payment.PaymentID, FromStatus: &prev_status, ToStatus: "VOIDED",})
 	if err != nil{
 		return models.Payment{},  fmt.Errorf("Unable to store voided transaction to state history db: %w", err)
+	}
+	
+	err = state.repository.CreateIdempotencyKey(key, res_payment.PaymentID)
+	if err != nil {
+		return models.Payment{}, fmt.Errorf("failed to store idempotency key: %w", err)
 	}
 
 	return res_payment, nil
