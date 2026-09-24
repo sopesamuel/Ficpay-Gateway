5f429109f968b5878527b7271c6af5bf441fa42f
sopesamuel <fasusimosope@gmail.com>
2026-09-24 11:41:02 +0200

    fix: reorganize handler functions and remove redundant authorization function

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -3,23 +3,34 @@ import (
 	"net/http"
 )
 
+type Handler struct {
+	service HandlerInterface
+}
+
+func NewHandler(service HandlerInterface) *Handler{
+	return &Handler{service: service}
+}
+
 //handlers for capture, void, refund, authorization
+func authorizationRequestFromFicmart(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func captureRequestFromFicmart(w http.ResponseWriter, r *http.Request) {
 
 }
 func voidRequestFromFicmart(w http.ResponseWriter, r *http.Request) {
 
 }
+
 func refundRequestFromFicmart(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func authorizationRequestFromFicmart(w http.ResponseWriter, r *http.Request) {
-
-}
 func getPaymentStatus(w http.ResponseWriter, r *http.Request) {
 
 }
+
 func getPaymentHistory(w http.ResponseWriter, r *http.Request) {
 
 }
