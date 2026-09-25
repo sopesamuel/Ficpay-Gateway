7cd62625bb37036969ee271b08eabe992ba1b707
sopesamuel <fasusimosope@gmail.com>
2026-09-25 10:28:56 +0200

    fix: add error handling for void and refund request handlers

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -73,11 +73,59 @@ func (h *Handler) captureRequestFromFicmart(w http.ResponseWriter, r *http.Reque
 
 
 func (h *Handler) voidRequestFromFicmart(w http.ResponseWriter, r *http.Request) {
-	
+	var req models.PaymentRequest
+
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	key := r.Header.Get("Idempotency-key")
+	if key == ""{
+		http.Error(w, "missing idempotency key", http.StatusBadRequest)
+		return
+	}
+
+
+
+	voidedpayment, err := h.service.VoidPayment(req.PaymentID, key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadGateway)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(voidedpayment)
 }
 
 func (h *Handler) refundRequestFromFicmart(w http.ResponseWriter, r *http.Request) {
+	var req models.PaymentRequest
 
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	key := r.Header.Get("Idempotency-key")
+	if key == ""{
+		http.Error(w, "missing idempotency key", http.StatusBadRequest)
+		return
+	}
+
+
+
+	refundedpayment, err := h.service.RefundPayment(req.PaymentID, key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadGateway)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(refundedpayment)
 }
 
 func (h *Handler) getPaymentStatus(w http.ResponseWriter, r *http.Request) {
